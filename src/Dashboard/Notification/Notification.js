9e2d87cacf08a047ccb112b0bbb3e197272f5e67
unknown <email@example.com>
2020-09-02 01:26:14 +0545

    new-commit

--- src/Dashboard/Notification/Notification.js
+++ src/Dashboard/Notification/Notification.js
@@ -1,9 +1,72 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useHistory } from "react-router-dom";
 import Alert from "../../components/Alert/Alert";
+import Axios from "axios"
+import { useAuth } from "../../Context/Auth";
 
 export default function Notification() {
 	let History = useHistory();
+	const {Authtoken} = useAuth()
+	const [seenNotification, setSeenNotification] = useState([])
+	const [unseenNotification, setUnseenNotification] = useState([])
+	const getUrl = "https://noname.dotnep.com/api/notifications"
+	const [err, setErr] = useState()
+
+	useEffect(() => {
+		let source = Axios.CancelToken.source();
+		const loadData = async () => {
+			try {
+				const response = await Axios.get(
+					getUrl, 
+					{
+						headers: {
+							Authorization: "bearer" + Authtoken.token
+						},
+						timeout: 10000
+					},
+					{
+						cancelToken: source.token
+					}
+				);
+				setSeenNotification(response.data.seen_notifications);
+				setUnseenNotification(response.data.unseen_notifications);
+				if(response.data.unseen_notifications.length > 0){
+					const unseen_id = []
+					response.data.unseen_notifications.map((noti,index)=>{return(
+						unseen_id.push(noti.id)
+					)})
+					console.log(unseen_id)
+					// const formData = new FormData();
+					// formData.append("unseen_ids", JSON.stringify(unseen_id));
+					// console.log(...formData)
+					Axios({
+						method: "post",
+						headers: { Authorization: "bearer" + Authtoken.token },
+						url: "https://noname.dotnep.com/api/notifications/seen",
+						data: {
+							unseen_id: JSON.stringify(unseen_id)
+						}
+						}).then(response => {
+						console.log(response)
+					});
+				}
+			}catch (error) {
+				if (Axios.isCancel(error)) {
+					console.log(error);
+				} else {
+					setErr(()=>{
+						throw error;
+					})
+				}
+			}
+		};
+		loadData();
+		return () => {
+			source.cancel();
+		};
+	}, [getUrl]);
+
+	
 	return (
 		<React.Fragment>
 			<div className="main-subject-containter">
@@ -24,34 +87,38 @@ export default function Notification() {
 				<Alert message={"Notification Received"} />
 				<div className="subject-content">
 					<div className="notificationWrapper">
+						
+						{unseenNotification.length > 0 && unseenNotification.map((notification, index)=>{return(
 						<div className="notification-item ">
 							<div className="close-item">
 								<span>
 									<i className="fa fa-window-close"></i>
 								</span>
 							</div>
-							<p>
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-								Obcaecati, unde culpa? Totam earum reiciendis quae itaque,
-								possimus magni asperiores, dignissimos voluptas adipisci eveniet
-								unde! Veniam deleniti illo quis quisquam quod?
+							<p dangerouslySetInnerHTML={{
+														__html:
+															notification.notifications
+													}}>
 							</p>
-							<div className="date">Feb 22, 4:00 PM <span className="seen-identify"> <i className="fa fa-eye"></i>  </span> <span className="new-identify"> new  </span></div>
+							<div className="date">{notification.date} <span className="seen-identify"> <i className="fa fa-eye"></i>  </span> <span className="new-identify"> new  </span></div>
 						</div>
+						)})}
+						{seenNotification.length > 0 && seenNotification.map((notification, index)=>{return(
 						<div className="notification-item seen">
 							<div className="close-item">
 								<span>
 									<i className="fa fa-window-close"></i>
 								</span>
 							</div>
-							<p>
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-								Obcaecati, unde culpa? Totam earum reiciendis quae itaque,
-								possimus magni asperiores, dignissimos voluptas adipisci eveniet
-								unde! Veniam deleniti illo quis quisquam quod?
+							<p dangerouslySetInnerHTML={{
+														__html:
+															notification.notifications
+													}}>
 							</p>
-							<div className="date">Feb 22, 4:00 PM <span className="seen-identify"> <i className="fa fa-eye"></i>  </span> <span className="new-identify"> new  </span></div> 
+							<div className="date">{notification.date} <span className="seen-identify"> <i className="fa fa-eye"></i>  </span> <span className="new-identify"> new  </span></div>
 						</div>
+						)})}
+						
 					</div>
 				</div>
 			</div>
